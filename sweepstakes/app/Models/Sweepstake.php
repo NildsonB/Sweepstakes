3188b82583183f201aa379fd7f4f747ff278e03f
Nildson <nildsonb15@gmail.com>
2025-10-30 23:18:49 -0300

    Create model sweepstake and including relationships

---
+++ sweepstakes/app/Models/Sweepstake.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sweepstake extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'number_of_winners',
+        'end_date',
+        'description'
+    ];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            if(empty($model->id)) {
+                $model->id = Str::uuid();
+            }
+        });
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
