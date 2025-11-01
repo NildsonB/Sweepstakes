51850d269c5d93c52a8a177fd05de6ca8bf57ed9
Nildson <nildsonb15@gmail.com>
2025-10-31 21:54:40 -0300

    Fix migrations user and participants

--- sweepstakes/database/migrations/2025_10_30_030757_create_participants_table.php
+++ sweepstakes/database/migrations/2025_10_30_030757_create_participants_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('participants', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->string('name');
             $table->string('email');
             $table->dateTime('awarded_at')->nullable();
